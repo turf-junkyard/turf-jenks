e117de4f21717d0db8c11fa372e1d4d2c4f62b78
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-05-13 21:43:40 -0400

    refactor first pass

---
+++ index.js
@@ -0,0 +1,15 @@
+var ss = require('simple-statistics'),
+
+module.exports = function(fc, field, num){
+  var vals = []
+  var breaks = []
+
+  fc.features.forEach(function(feature){
+    if(!(feature.properties[field]===undefined)){
+      vals.push(feature.properties[field])
+    }
+  })
+  breaks = ss.jenks(vals, num)
+
+  return breaks;
+}
