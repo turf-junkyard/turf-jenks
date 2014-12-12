c7c979b46eaed5f5205fab8b068e92bb7df19cd0
morganherlocker <morgan.herlocker@gmail.com>
2014-12-12 10:50:39 -0500

    ;s

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var ss = require('simple-statistics')
+var ss = require('simple-statistics');
 
 module.exports = function(fc, field, num){
-  var vals = []
-  var breaks = []
+  var vals = [];
+  var breaks = [];
 
   fc.features.forEach(function(feature){
     if(!(feature.properties[field]===undefined)){
-      vals.push(feature.properties[field])
+      vals.push(feature.properties[field]);
     }
-  })
-  breaks = ss.jenks(vals, num)
+  });
+  breaks = ss.jenks(vals, num);
 
   return breaks;
 }
