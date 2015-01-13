d2470fc926031d69f1d5e4024a5c161b33c67f17
Tom MacWright <tom@macwright.org>
2015-01-12 19:04:34 -0500

    Add JSDoc

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 var ss = require('simple-statistics');
 
+/**
+* Given a FeatureCollection, return the [Jenks Natural breaks](http://en.wikipedia.org/wiki/Jenks_natural_breaks_optimization)
+* of a given property
+* @module turf/jenks
+* @param {FeatureCollection} input
+* @param {string} field
+* @param {number} numberOfBreaks
+* @return {Array<number>} the number of breaks
+* @example
+* var points = turf.featurecollection([
+*   turf.point(5,5, {population: 200}),
+*   turf.point(1,3, {population: 600}),
+*   turf.point(14,2, {population: 100}),
+*   turf.point(13,1, {population: 200}),
+*   turf.point(19,7, {population: 300})]);
+* var breaks = turf.jenks(points, 'population', 2);
+* //=breaks
+*/
 module.exports = function(fc, field, num){
   var vals = [];
   var breaks = [];
 
   fc.features.forEach(function(feature){
-    if(!(feature.properties[field]===undefined)){
+    if(feature.properties[field]!==undefined){
       vals.push(feature.properties[field]);
     }
   });
   breaks = ss.jenks(vals, num);
 
   return breaks;
-}
+};
