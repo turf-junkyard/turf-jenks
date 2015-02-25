0e7e4c93eb607f226ccb6cb17100d48018839d86
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-25 11:51:29 -0800

    Switch doc to closure compiler templating

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var ss = require('simple-statistics');
 
 /**
-* Takes a {@FeatureCollection} of any type and returns an array of the [Jenks Natural breaks](http://en.wikipedia.org/wiki/Jenks_natural_breaks_optimization)
+* Takes features and returns an array of the [Jenks Natural breaks](http://en.wikipedia.org/wiki/Jenks_natural_breaks_optimization)
 * for a given property
 * @module turf/jenks
 * @category classification
-* @param {FeatureCollection} input a FeatureCollection of any type
-* @param {string} field the property in `input` on which to calculate Jenks natural breaks
-* @param {number} numberOfBreaks number of classes in which to group the data
+* @param {FeatureCollection<(Point|LineString|Polygon)>} input a FeatureCollection of any type
+* @param {String} field the property in `input` on which to calculate Jenks natural breaks
+* @param {Number} numberOfBreaks number of classes in which to group the data
 * @return {Array<number>} the break number for each class plus the minimum and maximum values
 * @example
 * var points = {
