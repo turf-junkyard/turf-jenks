234dc282e791b141a6cd4333cca6b3f4bc517a1d
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-26 12:37:21 -0800

    remove types from inputs that take all types

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var ss = require('simple-statistics');
 
 /**
-* Takes features and returns an array of the [Jenks Natural breaks](http://en.wikipedia.org/wiki/Jenks_natural_breaks_optimization)
+* Takes a set of features and returns an array of the [Jenks Natural breaks](http://en.wikipedia.org/wiki/Jenks_natural_breaks_optimization)
 * for a given property
 * @module turf/jenks
 * @category classification
-* @param {FeatureCollection<(Point|LineString|Polygon)>} input a FeatureCollection of any type
+* @param {FeatureCollection} input input features
 * @param {String} field the property in `input` on which to calculate Jenks natural breaks
 * @param {Number} numberOfBreaks number of classes in which to group the data
 * @return {Array<number>} the break number for each class plus the minimum and maximum values
